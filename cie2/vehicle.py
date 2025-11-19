6a891ac29da2ddbbd1b4f3486bd5d00a91f21368
Deepamk1 <mkdeepa758@gmail.com>
2025-11-19 11:12:57 +0530

    Update vehicle.py

--- cie2/vehicle.py
+++ cie2/vehicle.py
@@ -1,21 +1,21 @@
 import sys
 
-def main():
-    if len(sys.argv) != 5:
-        print("Usage: python vehicle_registration.py <car_number> <owner_name> <vehicle_type> <rc_expiry_year>")
-        sys.exit(1)
-
+if len(sys.argv) == 5:
     car_number = sys.argv[1]
     owner_name = sys.argv[2]
     vehicle_type = sys.argv[3]
     rc_expiry_year = sys.argv[4]
+    print("User inputs provided")
+else:
+    car_number = "KA01AB1234"
+    owner_name = "xyz"
+    vehicle_type = "abc"
+    rc_expiry_year = "2025"
+    print("Default inputs used")
 
-    print("\n--- Vehicle Registration Info ---")
-    print(f"Car Number       : {car_number}")
-    print(f"Owner Name       : {owner_name}")
-    print(f"Vehicle Type     : {vehicle_type}")
-    print(f"RC Expiry Year   : {rc_expiry_year}")
-    print("--------------------------------")
-
-if __name__ == "__main__":
-    main()
+print("Vehicle Registration Info:")
+print("-------------------------")
+print(f"Car Number: {car_number}")
+print(f"Owner Name: {owner_name}")
+print(f"Vehicle Type: {vehicle_type}")
+print(f"RC Expiry Year: {rc_expiry_year}")
